307fea40296cca498fd39125ad17694a391ebe50
Lev <lzibarev@yandex.ru>
2013-05-28 15:21:01 +0400

    rollback

--- Tournament/src/ru/lev/t1000/Task.java
+++ Tournament/src/ru/lev/t1000/Task.java
@@ -3,6 +3,7 @@ package ru.lev.t1000;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
+/***/
 public class Task {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
